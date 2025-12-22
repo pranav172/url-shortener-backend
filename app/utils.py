64adb95a36d1abba4fb770b549b24ecfddaf208e
Pranav Raj <rloveumom@gmail.com>
2025-12-23 04:02:31 +0530

    feat: implement Base62 encoding utility

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encode_base62(num: int) -> str:
+    if num == 0:
+        return BASE62[0]
+
+    result = []
+    while num > 0:
+        num, rem = divmod(num, 62)
+        result.append(BASE62[rem])
+
+    return "".join(reversed(result))
