3a2a5df3abd282c9fabf86c1bc0dbf414ee63118
Pranav Raj <rloveumom@gmail.com>
2025-12-24 23:47:29 +0530

    feat: add IP-based rate liminting to shorten endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+
+import time
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
+from collections import defaultdict
+from fastapi import Request
 from app.database import engine, SessionLocal, Base
 from app import models
 from app.models import URL
@@ -12,6 +16,26 @@ from app.utils import encode_base62
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="URL Shortener")
+
+RATE_LIMIT = 5        # max requests
+WINDOW_SIZE = 60      # seconds
+
+request_log = defaultdict(list)
+
+def check_rate_limit(ip: str):
+    current_time = time.time()
+    window_start = current_time - WINDOW_SIZE
+
+    # Remove old timestamps
+    request_log[ip] = [
+        t for t in request_log[ip] if t > window_start
+    ]
+
+    if len(request_log[ip]) >= RATE_LIMIT:
+        return False
+
+    request_log[ip].append(current_time)
+    return True
 
 # Dependency
 def get_db():
@@ -28,9 +52,20 @@ def health():
 
 # Create short URL
 @app.post("/shorten", response_model=URLResponse)
-def shorten_url(payload: URLCreate, db: Session = Depends(get_db)):
-    url = URL(original_url=str(payload.original_url))
+def shorten_url(
+    payload: URLCreate,
+    request: Request,
+    db: Session = Depends(get_db)
+):
+    client_ip = request.client.host
 
+    if not check_rate_limit(client_ip):
+        raise HTTPException(
+            status_code=429,
+            detail="Too many requests. Try again later."
+        )
+
+    url = URL(original_url=str(payload.original_url))
     db.add(url)
     db.commit()
     db.refresh(url)
