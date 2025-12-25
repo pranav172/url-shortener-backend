51b392f0055d52170ca0eb7ab13a963371d92361
Pranav Raj <rloveumom@gmail.com>
2025-12-25 07:22:28 +0530

    chore: read database and redis config from env

--- app/cache.py
+++ app/cache.py
@@ -1,7 +1,12 @@
 import redis
 
-redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    decode_responses=True  # return strings, not bytes
+# redis_client = redis.Redis(
+#     host="localhost",
+#     port=6379,
+#     decode_responses=True  # return strings, not bytes
+# )
+import os
+redis_client = redis.Redis.from_url(
+    os.getenv("redis://red-d569ebur433s73e1iof0:6379"),
+    decode_responses=True
 )
--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:Loveumaa@localhost:5432/url_shortener"
+import os
+DATABASE_URL = "postgresql://url_shortener_db_rmo3_user:wqutYMBbxeNxrtIz8WwrssCaQ8hppHWx@dpg-d569bk3uibrs739fucb0-a/url_shortener_db_rmo3"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
